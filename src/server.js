c426f1c82b61198a59e29569c83f76a530c0ecdb
b00db <48580026+b00db@users.noreply.github.com>
2022-01-24 13:19:51 +0900

    Create trello.css

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+app.use(express.static(__dirname + "/"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/trello.html");
 });
